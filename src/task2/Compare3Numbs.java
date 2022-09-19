416ea00e5c3c2d04916ca1e3f330fc108e06fb85
Maksym Gavryshenko <maksym.gavryshenko@ext.checkr.com>
2022-09-19 14:38:07 +0300

    comments resolved

--- src/task2/Compare3Numbs.java
+++ src/task2/Compare3Numbs.java
@@ -16,11 +16,12 @@ public class Compare3Numbs {
     }
 
     public static int compareNumbers(int num1, int num2, int num3) {
-        if (num3 > num2 && num3 > num1) {
-            return num3;
+        if (num1 > num2 && num1 > num3) {
+            return num1;
         } else if (num2 > num1 && num2 > num3) {
             return num2;
-        } else
-            return num1;
+        } else {
+            return num3;
+        }
     }
 }
--- src/task2/DigitsSum.java
+++ src/task2/DigitsSum.java
@@ -7,14 +7,14 @@ public class DigitsSum {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter integer number");
         int number = sc.nextInt();
-        System.out.println("Sum of the digits in number " + number + " is " + calcNumDigitSum(number));
+        System.out.printf("Sum of the digits in number %d is %d", number, calcNumDigitSum(number));
     }
 
     public static int calcNumDigitSum(int number) {
         int sum = 0;
         while (number > 0) {
-            sum = sum + number % 10;
-            number = number / 10;
+            sum += number % 10;
+            number /= 10;
         }
         return sum;
     }
