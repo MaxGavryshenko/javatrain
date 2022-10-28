95ce4f538af70d6111aa160252d0791c39b97bb5
Maksym Gavryshenko <maksym.gavryshenko@ext.checkr.com>
2022-10-28 11:54:57 +0300

    Home task in progress

---
+++ src/oop_polymorphysm/task1/Drum.java
@@ -0,0 +1,6 @@
+package oop_polymorphysm.task1;
+
+public class Drum extends Instrument implements MakeSound{
+    public static void
+
+}
---
+++ src/oop_polymorphysm/task1/MakeSound.java
@@ -0,0 +1,7 @@
+package oop_polymorphysm.task1;
+
+public interface MakeSound {
+    public static void playSound(){
+
+    }
+}
